897d2706dca016b6a7da34b831338ac4addb2610
unknown <black_pearl_from971@hotmail.comuse.name>
2019-10-18 13:51:07 +0200

    dashboard aff OK

--- admin/functions/dashboard-func.php
+++ admin/functions/dashboard-func.php
@@ -1,9 +1,44 @@
 <?php
 
-function inTable($table){
+function inTable($table)
+{
 
     global $db;
-    $req= $db->query("SELECT COUNT(id.$table) FROM $table");
-    var_dump($req);
-    return $nombre= $req->fetch(); // fetch nous renvera juste le nombre que l'on aura à afficher
+    $query = $db->query("SELECT COUNT(*) FROM $table");
+    // var_dump($query);
+    return $nombre = $query->fetch(); // fetch nous renvera juste le nombre que l'on aura à afficher
+}
+
+function getColor($table, $colors)
+{
+    if (isset($colors[$table])) {
+        return $colors[$table];
+    } else {
+        return "dark";
+    }
+}
+
+function get_Comments()
+{
+
+    global $db;
+    $req = $db->query("SELECT commentaire.idcom,
+            commentaire.login,
+            commentaire.email,
+            commentaire.date,
+            commentaire.idarticle,
+            commentaire.contenu,
+            articles.titre
+    FROM commentaire
+    JOIN articles
+    ON commentaire.idarticle = articles.idarticle
+    WHERE commentaire.modere='0'
+    ORDER BY commentaire.date ASC
+    ");
+
+    $results = [];
+    while ($rows = $req->fetchObject()) {
+        $results[] = $rows;
+    }
+    return $results;
 }
--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -11,18 +11,24 @@ include 'pages/header.php';
         "Administrateurs" => "users"
 
     ];
+
+    $colors = [
+        "articles" => "info",
+        "commentaire" => "success",
+        "users" => "danger"
+    ];
     ?>
     <?php
-    foreach ($tables as $table_name => $table) {// foreach parcour le tableau dont la clé est le nom (publications) et dont le contenu est dans la base de donnée (articles)
+    foreach ($tables as $table_name => $table) { // foreach parcour le tableau dont la clé est le nom (publications) et dont le contenu est dans la base de donnée (articles)
         ?>
         <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card bg-primary">
+            <div class="card bg-<?= getColor($table, $colors) ?>">
                 <div class="card-body text-white">
-                    <span class="card-title"><?= $table_name?></span>
+                    <span class="card-title"><?= $table_name ?></span>
                     <?php
-                    $nbrInTable=inTable($table);
-                    ?>
-                    <h4><?=$nbrInTable[0]?></h4> 
+                        $nbrInTable = inTable($table);
+                        ?>
+                    <h4><?= $nbrInTable[0] ?></h4>
                 </div>
             </div>
         </div>
@@ -32,6 +38,40 @@ include 'pages/header.php';
     ?>
 
 </div>
+<h4>Commentaires non-lus</h4>
+<?php
+$comments = get_Comments();
+?>
+<table class="table "style ="color:#575656">
+    <thead >
+        <tr>
+            <th scope="col">Article</th>
+            <th scope="col">Commentaire</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+       
+            <?php
+            foreach ($comments as $comment) {
+                ?>
+                <tr scope="row" id="commentaire_<?=$comment->idcom?>">
+                <td ><?=$comment->titre?></td>
+                <td ><?= substr($comment->contenu,0,50);?>...</td>
+                <td >
+                    <a href="#" id="<?=$comment->idcom?>" class=" btn-"></a>
+
+
+                </td>
+            <?php
+            }
+
+            ?>
+
+        </tr>
+    </tbody>
+
+</table>
 
 
 <pre>
