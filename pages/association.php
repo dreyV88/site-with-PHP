addb4be8b7522d4f5a9407a1cce6b069c8b76834
unknown <black_pearl_from971@hotmail.comuse.name>
2019-10-28 12:01:34 +0100

    (front) page association

---
+++ pages/association.php
@@ -0,0 +1,110 @@
+<?php
+include 'pages/header.php';
+?>
+
+<div class="row">
+    <div class="col-lg-3">
+        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="nav-link active" id="v-pills-asso-tab" data-toggle="pill" href="#asso" role="tab" aria-controls="asso" aria-selected="true">Création</a>
+            <a class="nav-link" id="v-pills-patrimoine-tab" data-toggle="pill" href="#patrimoine" role="tab" aria-controls="patrimoine" aria-selected="false">Patrimoine</a>
+            <a class="nav-link" id="v-pills-objectifs-tab" data-toggle="pill" href="#objectifs" role="tab" aria-controls="objectifs" aria-selected="false">Objectifs</a>
+            <!-- <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a> -->
+        </div>
+    </div>
+    <div class="col-lg-9">
+        <div class="tab-content" id="vertical-tabContent">
+            <div class="tab-pane fade show active" id="asso" role="tabpanel" aria-labelledby="v-pills-asso-tab">
+                <h2>La Création</h2>
+                <p>L'association les amis du clocher existe depuis le 15 octobre 2011.
+                    Elle a vu le jour dans le cadre de la volonté de restaurer le patrimoine communal dont l'église Saint Géry est la pièce maîtresse
+                </p>
+                <h3>Son but</h3>
+                <p>
+                    Son but es de contribuer à la restauration et à la valorisation du patrimoine du village, et dans l'urgence à la restauration de l'église labellisée "monument remarquable".
+                    Nous apportons notre soutien à la municipalité en participant aux actions en cours.
+                    Avec l'aide des habitants, particuliers et professionnels, de la commune et des communes voisines,
+
+                    Pour sauvegarder l'église, le projet de restauration bénéficie des subventions de la Région, du Département, de l’État (DETR et Réserve
+                    Parlementaire), du fond de concours de la Communauté d'Agglomération de Cambrai (CAC), de la Fondation du Patrimoine et des fonds
+
+                    propres de la Municipalité.
+
+                    En complément de ces financements qui ne couvrent pas la totalité du projet de restauration, l'association engage des actions afin de
+
+                    collecter, en partie, le reliquat nécessaire au financement total des travaux de remise en état
+
+                    Campagne médiatique et appel aux dons
+
+                    Identification de mécènes potentiels et émission de bons de souscription
+
+                    Manifestations festives et culturelles.
+
+                    En ce qui concerne la collecte de dons avec reçus fiscaux, des conventions établies entre la Fondation du Patrimoine, la Mairie et notre
+
+                    Association permettent
+
+                    l'ouverture de souscriptions pilotées par la Fondation du Patrimoine
+
+                    via son site internet
+
+                    www.nord-pas-de-calais.fondation-patrimoine.org 
+                    (EGLISE SAINT GERY DE CAGNONCLES ).
+
+                    A noter la mobilisation et la participation exceptionnelle de tous les Cagnonclaises et Cagnonclais
+
+                    et des amis des communes voisines via les adhésions, les dons, les entraides et contributions diverses aux manifestations festives et
+
+                    culturelles
+                </p>
+                <h3>Le Logo</h3>
+                <p>Une petite et belle image.Reflet d'un très joli endroit
+                    Dessine moi un VILLAGE.Un CLOCHER, quelques TOITS.</p>
+                <img src="images/DSC00232.JPG" width="25%" alt="logo les amis du clocher">
+                <h3>Le Bureau</h3>
+                <p>La Secrétaire : Marie Ange Soeur épouse Zimmer,
+
+                    le Président : Emile Pinoy,
+                    la Secrétaire adjointe : Françoise Bury,
+                    le Trésorier adjoint : Alain Delfolie
+                    La Vice – Présidente : Michèle Quinchon,
+
+                    la Trésorière : Elisabeth Mignot épouse Dellemotte</p>
+
+            </div>
+            <div class="tab-pane fade" id="patrimoine" role="tabpanel" aria-labelledby="v-pills-patrimoine-tab">
+
+            </div>
+            <div class="tab-pane fade" id="objectifs" role="tabpanel" aria-labelledby="v-pills-objectifs-tab">
+                <p>
+                    Quelques mots sur notre programme 2014
+                    Nous proposons en 2014 un repas dansant le 27 avril et la reconduction de la Saint Sylvestre.
+                    Un spectacle « sons et lumières » à Cagnoncles ? Pourquoi pas !
+
+                    Il faut se lancer pour voir
+                    Cet événement autour de l'histoire du village est en préparation.
+                    Il serait programmé en septembre 2014 ou 2015 en fonction de l'avancement des travaux de deuxième phase et de la fin des répétitions
+                    de mise au point du spectacle.
+                    Après l'écriture et le cadencement d'une première approche la plus exhaustive possible a propos des dialogues, des saynètes, des
+                    musiques, des chansons, des projections et des jeux de lumières, commencera, dès le début du mois de décembre, une longue phase
+                    de prospection afin
+                    *de trouver des personnes compétentes dans le domaine « sons et lumières » pour nous aider,
+                    *de rechercher des figurants, des comédiens, afin de définir, concevoir et confectionner ou louer les costumes,
+                    *de mettre en scène,
+                    *de cerner et de se procurer le matériel nécessaire .
+                    Et, finalement, de trouver la meilleure formule pour que le spectacle soit le plus conforme possible à nos rêves malgré les réalités
+                    économiques et techniques qui ne vont pas manquer d'entraver certains effets souhaités.
+                    Nous envisageons en 2015 ou 2016, la rénovation et la décoration de l'intérieur de l'Eglise après la troisième phase de travaux.
+                    Nous nous efforcerons de retrouver, en partie, la décoration existante en début du XX° siècle
+                    Par exemple, l'ancien chemin de croix est en phase finale de restauration au niveau des toiles.
+                    Les cadres seront nettoyés, réparés. Un nouveau cadre a été entièrement refait
+                    A noter que suite à l'exposition inter- associative d'octobre 2012 pilotée par l'ASPEC (Association de Sauvegarde du Patrimoine et de
+                    l'Environnement du Cambrésis), un comité inter-associations pour la mémoire du Cambrésis a été créé. Notre association en tant que
+                    membre de ce comité participera du 18 au 24 octobre 2014 à une exposition consacrée au centenaire de la guerre 1914 - 1918.
+                    En l'occurrence, si vous souhaitez contribuer à nos recherches relatives à la vie de notre village pendant la période de la première guerre
+                    mondiale, nous sommes preneurs d'informations, de photos, de lettres de soldats ou d'autres aspects (Tél 06 88 30 43 18).
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
